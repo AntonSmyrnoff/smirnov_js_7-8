e3ba76f433debef26c197459c9d2a45433dabcf6
AntonSmyrnoff <a.smyrnoff@gmail.com>
2016-12-10 00:36:41 +0200

    both tasks are done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(function() {
-	console.log($)
 	var $tabs = $('.tabs');
 	$($tabs).click(function() {
 		
@@ -11,7 +10,32 @@ $(function() {
 
 		$($text[$indexTab]).addClass('tab-text--active');
 		$($text[$indexTab]).siblings().removeClass('tab-text--active');
+	});
+
+	//TASK2
+	var $input = $('input');
+	$($input).mouseover(function() {
+		$(this).siblings('div').fadeIn(400);
+	});
+
+	$($input).mouseout(function() {
+		$(this).siblings('div').fadeOut(50);
+	});	
+	
+	$($input).focus(function() {
+		$(this).siblings('div').fadeIn(400);
 	})
+
+	$($input).blur(function() {
+		$(this).siblings('div').fadeOut(50);
+	});	
+
+	//ADD ALL TOOLTIPS ON BUTTONCLICK
+	$('.helpbutton').click(function() {
+		$('.tooltips').css('display', 'inline-block');
+	})	
+
+	
 })
 
 
