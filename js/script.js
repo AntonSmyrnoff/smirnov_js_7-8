e2d5fb06a41b7fe356a9f52b468ad2c05517fc8b
AntonSmyrnoff <a.smyrnoff@gmail.com>
2016-12-09 22:27:27 +0200

    first task is ready

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $(function() {
 	console.log($)
-	var $element = $('');
-	$element.addClass('')
+	var $tabs = $('.tabs');
+	$($tabs).click(function() {
+		
+		$(this).addClass('tab--active');
+		$(this).siblings().removeClass('tab--active');
+		var $indexTab = $($tabs).index(this);
+
+		$text = $('.tab-text');
+
+		$($text[$indexTab]).addClass('tab-text--active');
+		$($text[$indexTab]).siblings().removeClass('tab-text--active');
+	})
 })
 
 
